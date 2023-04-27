c09b21cbcaa10903a93fd5d7ad1f63c154306a02
Marina <demi87@mail.ru>
2023-04-28 01:40:04 +0700

    add allure

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,8 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,6 +13,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     public void shouldSuccessfulPlanAndReplanMeeting() {
